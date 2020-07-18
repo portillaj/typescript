9d1566545b0375b2c3de31d971ae754d321304e6
jasonportilla <jason.portilla85@gmail.com>
2020-07-18 15:04:20 -0400

    added changes

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -1,8 +1,10 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
   constructor(private data: T){}
 
-  get(propName: string): T {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
